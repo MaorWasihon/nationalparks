42d521285bb5eb5ba23abf9bcb51363bb32f1f48
MaorWasihon <62095365+MaorWasihon@users.noreply.github.com>
2022-02-02 15:05:03 +0200

    Update BackendController.java

--- src/main/java/com/openshift/evg/roadshow/parks/rest/BackendController.java
+++ src/main/java/com/openshift/evg/roadshow/parks/rest/BackendController.java
@@ -18,6 +18,6 @@ public class BackendController{
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
     public Backend get() {
          System.out.println("[INFO] Maor Wasihon()");
-        return new Backend("nationalparks","Maor Wasihon Park", new Coordinates("47.039304", "14.505178"), 4);
+        return new Backend("nationalparkws","Maor Wasihon Park", new Coordinates("47.039304", "14.505178"), 4);
     }
 }
